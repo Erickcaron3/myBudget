58aa2aa330dbff6686ba68e878320f4163c59fbf
Erick CARON <erick.caron3@gmail.com>
2021-05-24 18:15:21 +0200

    data.sql filled in

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,1 +1,2 @@
-
+insert into currencies(country, is_deletable, name, shortcut) VALUES ('Poland', true, 'zloty', 'PLN'),('Europe',true,'Euro','EUR'), ('England',TRUE,'Pound', 'GBP');
+insert into shops(category, is_deletable, name) VALUES ('Grocery',true,'Biedronka'),('Grocery',true,'Auchan'),('Grocery',true,'Carrefour'),('Grocery',true,'Lidl'),('Grocery',true,'Lewiatan'),('Grocery',true,'Tesco'),('Grocery',true,'Aldi'),('Grocery',true,'Zabka');
