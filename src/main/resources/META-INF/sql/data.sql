1fe24f9afbefe7eba9c2cf1404f82b5d7b744c8e
Erick CARON <erick.caron3@gmail.com>
2021-05-24 18:21:35 +0200

    data.sql filled in better

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,2 +1,5 @@
-insert into currencies(country, is_deletable, name, shortcut) VALUES ('Poland', true, 'zloty', 'PLN'),('Europe',true,'Euro','EUR'), ('England',TRUE,'Pound', 'GBP');
-insert into shops(category, is_deletable, name) VALUES ('Grocery',true,'Biedronka'),('Grocery',true,'Auchan'),('Grocery',true,'Carrefour'),('Grocery',true,'Lidl'),('Grocery',true,'Lewiatan'),('Grocery',true,'Tesco'),('Grocery',true,'Aldi'),('Grocery',true,'Zabka');
+insert into currencies(country, is_deletable, name, shortcut) VALUES ('Poland', true, 'zloty', 'PLN'),('Europe',true,'Euro','EUR'),('England',TRUE,'Pound', 'GBP');
+insert into shops(category, is_deletable, name) VALUES ('Grocery',true,'Biedronka'),('Grocery',true,'Auchan'),('Grocery',true,'Carrefour'),('Grocery',true,'Lidl'),('Grocery',true,'Tesco'),('Grocery',true,'Aldi'),('Grocery',true,'Zabka'),('RTV',true,'Euro RTV AGD'),('RTV',true,'Mediaexpert'),('RTV',true,'Mediamarket'),('RTV',true,'Neonet'),('Sport',true,'Decathlon'),('Sport',true,'Intersport'),('Sport',true,'3M');
+
+
+
